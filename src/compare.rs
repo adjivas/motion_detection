f9b1dd13a1018ea78f19d9b239e947c9b61e7823
navi <navi@42.fr>
2023-04-22 22:23:09 +0200

    add compare

---
+++ src/compare.rs
@@ -0,0 +1,14 @@
+use image::DynamicImage;
+use image_compare::prelude::*;
+
+pub fn compare(past: &DynamicImage, present: &DynamicImage) -> Result<f64, Box<dyn std::error::Error>> {
+    let distortion = image_compare::gray_similarity_structure(
+        &Algorithm::RootMeanSquared,
+        &past.to_luma8(),
+        &present.to_luma8()
+    )?;
+
+    println!("D: {}", distortion.score);
+
+    Ok(distortion.score)
+}
