ace8c6cb9b1392ee7dd3a579fefaede179864888
navi <navi@42.fr>
2023-04-23 15:24:22 +0200

    step 3

--- src/main.rs
+++ src/main.rs
@@ -2,52 +2,44 @@ mod request;
 mod compare;
 
 use std::error::Error;
-use request::req_send_image;
+use std::{time::Duration, thread::sleep};
+use std::iter;
 use compare::compare;
-use rumqttc::{self, Client, MqttOptions, QoS};
-use tokio::time::{sleep, Duration};
-use tokio::sync::mpsc::channel;
-use tokio::runtime::Runtime;
-
+use rumqttc::{Client, MqttOptions, QoS, Outgoing, Event};
+use request::get_image;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mqtt_name = Box::leak(std::env::var("MQTT_NAME").unwrap().into_boxed_str()) as &'static str;
     let mqtt_host = Box::leak(std::env::var("MQTT_HOST").unwrap().into_boxed_str()) as &'static str;
     let mqtt_port = Box::leak(std::env::var("MQTT_PORT").unwrap().into_boxed_str()) as &'static str;
+    let mqtt_port = mqtt_port.parse::<u16>().unwrap();
     let mqtt_publish = Box::leak(std::env::var("MQTT_PUBLISH").unwrap().into_boxed_str()) as &'static str;
     let http_cgi_host = Box::leak(std::env::var("HTTP_CGI_HOST").unwrap().into_boxed_str()) as &'static str;
     let http_cgi_user = Box::leak(std::env::var("HTTP_CGI_USER").unwrap().into_boxed_str()) as &'static str;
     let http_cgi_pass = Box::leak(std::env::var("HTTP_CGI_PASS").unwrap().into_boxed_str()) as &'static str;
     let http_cgi_interval = Duration::from_secs(1);
+    let mqtt_interval = Duration::from_secs(5);
 
-    let rt  = Runtime::new()?;
+    let mut mqtt_options = MqttOptions::new(mqtt_name, mqtt_host, mqtt_port);
+    mqtt_options.set_keep_alive(mqtt_interval);
+    let (mut client, mut eventloop) = Client::new(mqtt_options, 10);
+    
+    let mut it_images = iter::repeat_with(|| {
+        sleep(http_cgi_interval);
+        let ref buf = get_image(http_cgi_host, http_cgi_user, http_cgi_pass).unwrap();
 
-    let (sender, mut receiver) = channel(1);
-    rt.spawn(async move {
-        let build = reqwest::Client::new()
-            .get(http_cgi_host)
-            .basic_auth(http_cgi_user, Some(http_cgi_pass));
-        while let Some(build) = build.try_clone() {
-            req_send_image(build, &sender).await.unwrap();
-            sleep(http_cgi_interval).await;
-        }
-    });
+        image::load_from_memory(buf).unwrap()
+    }).peekable();
 
-    let port = mqtt_port.parse::<u16>().unwrap();
-
-    let options = MqttOptions::new(mqtt_name, mqtt_host, port);
-    let (mut client, mut eventloop) = Client::new(options, 2);
-
-    let ref before = receiver.blocking_recv().unwrap();
-    let mut before = image::load_from_memory(before)?;
-    while let Some(ref after) = receiver.blocking_recv() {
-        let after = image::load_from_memory(after)?;
-
+    loop {
+        let before = it_images.next().unwrap();
+        let after = it_images.peek().unwrap();
         let distortion = compare(&before, &after)?;
 
-        client.publish(mqtt_publish, QoS::AtLeastOnce, true, distortion.to_string().as_bytes()).unwrap();
-        eventloop.recv().unwrap()?;
-        before = after;
+        client.publish(mqtt_publish, QoS::AtMostOnce, false, distortion.to_string().as_bytes()).unwrap();    
+        eventloop.iter().take_while(|x| { dbg!(x); match x {
+            Ok(Event::Outgoing(Outgoing::Publish(_))) => false,
+            _ => true,
+        }}).for_each(drop);
     }
-    Ok(())
 }
--- src/request.rs
+++ src/request.rs
@@ -1,11 +1,10 @@
 use bytes::Bytes;
-use reqwest::RequestBuilder;
-use tokio::sync::mpsc::Sender;
 use std::error::Error;
 
-pub async fn req_send_image(client: RequestBuilder, sender: &Sender<Bytes>) -> Result<(), Box<dyn Error>> {
-    let buf = client.send().await?
-        .bytes().await?;
+pub fn get_image(http_cgi_host: &str, http_cgi_user: &str, http_cgi_pass: &str) -> Result<Bytes, Box<dyn Error>> {
+    let request = reqwest::blocking::Client::new()
+        .get(http_cgi_host)
+        .basic_auth(http_cgi_user, Some(http_cgi_pass));
 
-    Ok(sender.send(buf).await?)
+    Ok(request.send()?.bytes()?)
 }
