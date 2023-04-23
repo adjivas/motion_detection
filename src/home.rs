5199827858001c6243ca7cd2722e35d9bf920600
navi <navi@42.fr>
2023-04-23 02:21:36 +0200

    remove MQTT subscription

--- src/home.rs
+++ src/home.rs
@@ -1,31 +1,17 @@
 use bytes::Bytes;
 use reqwest::Client;
-use tokio::sync::{RwLock, mpsc::Sender};
-use std::{error::Error, sync::Arc};
-
-type Lock = Arc<RwLock<bool>>;
+use tokio::sync::mpsc::Sender;
+use std::error::Error;
 
 static HTTP_CGI_HOST: &'static str = dotenv!("HTTP_CGI_HOST");
 static HTTP_CGI_USER: &'static str = dotenv!("HTTP_CGI_USER");
 static HTTP_CGI_PASS: &'static str = dotenv!("HTTP_CGI_PASS");
-pub static ABSENT: bool = false;
-pub static PRESENT: bool = !ABSENT;
 
-pub async fn send_image(sender: &Sender<Bytes>) -> Result<(), Box<dyn Error>> {
+pub async fn req_send_image(sender: &Sender<Bytes>) -> Result<(), Box<dyn Error>> {
     let buf = Client::new()
         .get(HTTP_CGI_HOST)
         .basic_auth(HTTP_CGI_USER, Some(HTTP_CGI_PASS)).send().await?
         .bytes().await?;
 
     Ok(sender.send(buf).await?)
-}
-
-pub async fn update_presence(lock: &Lock, message: &Bytes) -> Result<(), Box<dyn Error>> {
-    let mut lock = lock.write().await;
-    if "present" == message {
-        *lock = PRESENT;
-    } else if "absent" == message {
-        *lock = ABSENT;
-    }
-    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,19 @@ extern crate dotenv_codegen;
 mod home;
 mod compare;
 
-use std::{error::Error, thread, time, sync::Arc};
-use home::{send_image, update_presence, ABSENT};
+use std::error::Error;
+use home::req_send_image;
 use lazy_static::lazy_static;
 use compare::compare;
-use rumqttc::{self, Client, AsyncClient, MqttOptions, QoS, Event, Incoming};
+use rumqttc::{self, Client, MqttOptions, QoS};
 use tokio::time::{self as TokioTime, Duration as TokioDuration};
-use tokio::sync::{RwLock, mpsc::channel};
+use tokio::sync::mpsc::channel;
 use tokio::runtime::Runtime;
 
 static MQTT_NAME: &'static str = dotenv!("MQTT_NAME");
 static MQTT_HOST: &'static str = dotenv!("MQTT_HOST");
 static MQTT_PORT: &'static str = dotenv!("MQTT_PORT");
-static MQTT_SUBSCRIBE: &'static str = dotenv!("MQTT_TOPIC_DOOR");
 static MQTT_PUBLISH: &'static str = dotenv!("MQTT_TOPIC_MOTION");
-static MQTT_INTERVAL: time::Duration = time::Duration::from_secs(1);
 static HTTP_CGI_INTERVAL: TokioDuration = TokioDuration::from_secs(1);
 
 lazy_static! {
@@ -32,34 +30,13 @@ lazy_static! {
 fn main() -> Result<(), Box<dyn Error>> {
     let rt  = Runtime::new()?;
 
-    let lock = Arc::new(RwLock::new(ABSENT));
-    let lock_cam = lock.clone();
-
     let (sender, mut receiver) = channel(2);
     rt.spawn(async move {
         let mut interval = TokioTime::interval(HTTP_CGI_INTERVAL);
 
         loop {
-            let is_locked = lock_cam.try_read().unwrap();
-            if *is_locked == ABSENT {
-                send_image(&sender).await.unwrap();
-            }
+            req_send_image(&sender).await.unwrap();
             interval.tick().await;
-        }
-    });
-
-    let (client, mut eventloop) = AsyncClient::new(MQTT_OPTIONS.clone(), 10);
-    rt.spawn(async move {
-        client
-            .subscribe(MQTT_SUBSCRIBE, QoS::AtMostOnce)    
-            .await    
-            .unwrap();
-
-        while let Ok(notification) = eventloop.poll().await {
-            if let Event::Incoming(Incoming::Publish(publish)) = notification {
-                update_presence(&lock, &publish.payload).await.unwrap();
-            }
-            thread::sleep(MQTT_INTERVAL);
         }
     });
 
